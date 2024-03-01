b6037de6955823f6c49c45b81580d11055f3b39e
Jimmy <jimmy.liu@mail.com>
2024-03-01 10:25:48 +0800

    create framework

---
+++ springboot/src/main/java/com/example/service/DoctorService.java
@@ -0,0 +1,135 @@
+package com.example.service;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.example.common.Constants;
+import com.example.common.enums.ResultCodeEnum;
+import com.example.common.enums.RoleEnum;
+import com.example.entity.Account;
+import com.example.entity.Doctor;
+import com.example.exception.CustomException;
+import com.example.mapper.DoctorMapper;
+import com.example.utils.TokenUtils;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 医生业务处理
+ **/
+@Service
+public class DoctorService {
+
+    @Resource
+    private DoctorMapper doctorMapper;
+
+    /**
+     * 新增
+     */
+    public void add(Doctor doctor) {
+        Doctor dbDoctor = doctorMapper.selectByUsername(doctor.getUsername());
+        if (ObjectUtil.isNotNull(dbDoctor)) {
+            throw new CustomException(ResultCodeEnum.USER_EXIST_ERROR);
+        }
+        if (ObjectUtil.isEmpty(doctor.getPassword())) {
+            doctor.setPassword(Constants.USER_DEFAULT_PASSWORD);
+        }
+        if (ObjectUtil.isEmpty(doctor.getName())) {
+            doctor.setName(doctor.getUsername());
+        }
+        doctor.setRole(RoleEnum.DOCTOR.name());
+        doctorMapper.insert(doctor);
+    }
+
+    /**
+     * 删除
+     */
+    public void deleteById(Integer id) {
+        doctorMapper.deleteById(id);
+    }
+
+    /**
+     * 批量删除
+     */
+    public void deleteBatch(List<Integer> ids) {
+        for (Integer id : ids) {
+            doctorMapper.deleteById(id);
+        }
+    }
+
+    /**
+     * 修改
+     */
+    public void updateById(Doctor doctor) {
+        doctorMapper.updateById(doctor);
+    }
+
+    /**
+     * 根据ID查询
+     */
+    public Doctor selectById(Integer id) {
+        return doctorMapper.selectById(id);
+    }
+
+    /**
+     * 查询所有
+     */
+    public List<Doctor> selectAll(Doctor doctor) {
+        return doctorMapper.selectAll(doctor);
+    }
+
+    /**
+     * 分页查询
+     */
+    public PageInfo<Doctor> selectPage(Doctor doctor, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<Doctor> list = doctorMapper.selectAll(doctor);
+        return PageInfo.of(list);
+    }
+
+    /**
+     * 登录
+     */
+//    public Account login(Account account) {
+//        Account dbDoctor = doctorMapper.selectByUsername(account.getUsername());
+//        if (ObjectUtil.isNull(dbDoctor)) {
+//            throw new CustomException(ResultCodeEnum.USER_NOT_EXIST_ERROR);
+//        }
+//        if (!account.getPassword().equals(dbDoctor.getPassword())) {
+//            throw new CustomException(ResultCodeEnum.USER_ACCOUNT_ERROR);
+//        }
+//        // 生成token
+//        String tokenData = dbDoctor.getId() + "-" + RoleEnum.ADMIN.name();
+//        String token = TokenUtils.createToken(tokenData, dbDoctor.getPassword());
+//        dbDoctor.setToken(token);
+//        return dbDoctor;
+//    }
+
+    /**
+     * 注册
+     */
+//    public void register(Account account) {
+//        Doctor doctor = new Doctor();
+//        BeanUtils.copyProperties(account, doctor);
+//        add(doctor);
+//    }
+
+    /**
+     * 修改密码
+     */
+//    public void updatePassword(Account account) {
+//        Doctor dbDoctor = doctorMapper.selectByUsername(account.getUsername());
+//        if (ObjectUtil.isNull(dbDoctor)) {
+//            throw new CustomException(ResultCodeEnum.USER_NOT_EXIST_ERROR);
+//        }
+//        if (!account.getPassword().equals(dbDoctor.getPassword())) {
+//            throw new CustomException(ResultCodeEnum.PARAM_PASSWORD_ERROR);
+//        }
+//        dbDoctor.setPassword(account.getNewPassword());
+//        doctorMapper.updateById(dbDoctor);
+//    }
+
+}
